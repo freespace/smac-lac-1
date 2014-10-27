9cd552c274d8a8fa7a9650f5e1fa6ccd9e29d3a5
Shuning Bian <freespace@gmail.com>
2014-10-27 16:00:35 +0000

    Updates move_abs and bench to use getport

--- bench.py
+++ bench.py
@@ -1,6 +1,8 @@
 from lac1 import LAC1
 
-lac1 = LAC1('/dev/ttyS0', baudRate=19200)
+from getport import getport
+lac1 = LAC1(getport(), baudRate=19200)
+
 lac1.home()
 
 lac1.set_max_velocity(5000)
--- move_abs.py
+++ move_abs.py
@@ -1,5 +1,6 @@
 from lac1 import LAC1
 
+from getport import getport
 
 if __name__ == '__main__':
   import sys
@@ -7,9 +8,9 @@ if __name__ == '__main__':
     print sys.argv[0],'<abs position in mm>'
     sys.exit(1)
   else:
-    lac1 = LAC1('/dev/ttyS0', baudRate=19200)
-    lac1.set_max_velocity(5000)
-    lac1.set_max_acceleration(1000)
+    lac1 = LAC1(getport(), baudRate=19200)
+    lac1.set_max_velocity(50)
+    lac1.set_max_acceleration(100)
     p = float(sys.argv[1])
     print 'Moving to',p,'mm'
     lac1.move_absolute_mm(p)
