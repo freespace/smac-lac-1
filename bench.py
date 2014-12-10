1d16e64e65f1f0c87f6b91a75d13c75ab19a330a
Shuning Bian <freespace@gmail.com>
2014-12-10 17:26:41 +0000

    Modifies bench.py to do stress test on sendcmds

--- bench.py
+++ bench.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import sys
 from lac1 import LAC1
 
 from getport import getport
@@ -5,20 +8,37 @@ lac1 = LAC1(getport(), baudRate=19200)
 
 lac1.home()
 
-lac1.set_max_velocity(5000)
+lac1.set_max_velocity(1000)
 lac1.set_max_torque(10000)
 lac1.set_max_acceleration(30000)
 #lac1.set_max_acceleration(5000)
 
 import time
-nloops = 50
-dist = 5
+nloops = 1000
+dist = 2
 
 lac1.move_absolute_mm(0)
 starttime = time.time()
 for cnt in xrange(nloops):
-  lac1.move_absolute_mm(dist)
-  lac1.move_absolute_mm(0)
+  try:
+    lac1.move_absolute_mm(dist, wait=False)
+    p = lac1.get_position_mm()
+    while p < dist:
+      p = lac1.get_position_mm()
+    
+    lac1.move_absolute_mm(0, wait=False)
+    p = lac1.get_position_mm()
+    while p > 0:
+      p = lac1.get_position_mm()
+
+
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    if cnt%100==0:
+      print cnt
+  except Exception, ex:
+    print 'Exception occured on loop %d'%(cnt+1), ex
+    break
 
 dt = time.time() - starttime
 
