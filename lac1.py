ee4341377b68e1d578f2c11dc9643f3c679bcd6c
Chris Muenzer <chris.muenzer@tenshi.co.in>
2026-04-23 11:37:32 +0200

    Updated macro number, recalculated values, stopped travel as soon as softland or travel is too far.

--- lac1.py
+++ lac1.py
@@ -543,63 +543,68 @@ class LAC1(object):
     """
     return self.sendcmds('TK', paramset)
   
-  def softland(self):
+  def softland(self, force=False, execute=True, limit=10, duty=0.9, mmpersecond=4, mmpersecondsquared=10000):
     """
-    This function executes a softland move
+    This function executes a softland move. Details are derived from SMAC-MCA 
+    Actuators User Manual rev 1.8 pg 38 
+    (https://www.smac-mca.com/documents/PDFs/SMAC%20Actuators%20User%20Manual.pdf)
+
+    Softland is the routine which enables the actuator to land on a surface 
+    with a low force, for example to measure a component. This is done in 
+    velocity mode, monitoring the position error as the rod is moving with a 
+    controlled force
     """
-    # go into velocity mode, turn motor on, set force, acceleration and
-    # velocity constants, set direction to be in the direction of INCREASING
-    # encoder count, start motion, wait 20ms.
-    #
-    # MD: define macro
-    # VM: velocity mode
-    # MN: motor on
-    # SQ: torque
-    # SA: acceleration
-    # SV: velocity
-    # DI: direction
-    # GO: begin movement
-    # WA: wait
-    self.sendcmds('MD100,VM,MN,SQ5000,SA1000,SV50000,DI0,GO,WA20')
+    enc_counts_per_mm = self.actuator.enc_counts_per_mm
 
-    # read word from memory 538, which is position error. If position error
-    # is greater than 20, display a message, jump to macro 105. If position error
-    # is greater than 5000, display a message, jump to macro 110. Otherwise repeat.
-    #
-    # MD: define macro
-    # RW: read word from memory 538, where position error is stored
-    # IB: if below
-    # MG: print message
-    # MJ: jump to macro
-    # RP: repeat
-    self.sendcmds('MD101,RW538,IG20,MG"FOUND",MJ105,RL494,IG5000,MG"TOO FAR",MJ110,RP')
+    macro500 = self.sendcmds('TM500')
+    if len(macro500) == 0 or force:
 
-    # Stop motion, display message (:N means no carriage return after message), 
-    # Tell Position then jump to MD110.
-    #
-    # MD: define macro
-    # ST: stop
-    # MG: print message
-    # TP: tell position
-    # MJ: jump to macro
-    self.sendcmds('MD105,ST,MG"POSITION = ":N,TP,MJ110')
+      # go into velocity mode, turn motor on, set force, acceleration and
+      # velocity constants, set direction to be in the direction of INCREASING
+      # encoder count, start motion, wait 20ms.
+      #
+      # MD: define macro
+      # VM: velocity mode
+      # MN: motor on
+      # SQ: torque
+      # SA: acceleration
+      # SV: velocity
+      # DI: direction
+      # GO: begin movement
+      # WA: wait
+      SQ = int(duty * 32767)
+      print('SQ:', SQ)
+      SA = int(self.KA * mmpersecondsquared)
+      print('SA:', SA)
+      SV = int(self.KV * mmpersecond)
+      print('SV:', SV)
+      self.sendcmds(f'MD500,VM,MN,SQ{SQ},SA{SA},SV{SV},DI0,GO,WA200')
 
-    # Enter position mode, set acceleration and force higher. Issue go home (GH) command but 
-    # wait 50ms before ramping force up to maximum. This is needed because if the force is 
-    #increased immediately then any position error will be taken up, possibly deforming the 
-    # measured component. Wait after stop 100ms then jump back to MD100.
-    # MD: define macro
-    # PM: position mode
-    # SA: acceleration
-    # SV: velocity
-    # GH: go home
-    # WA: wait
-    # SQ: torque
-    # WS: wait stop
-    # MJ: jump to macro
-    self.sendcmds('MD110,PM,MN,SA5000,SV500000,GH,WA50,SQ32767,WS100,MJ100')
+      # read word from memory 538, which is position error. If position error
+      # is greater than 20, display a message, jump to macro 505. If position error
+      # is greater than 5000, display a message, jump to macro 510. Otherwise repeat.
+      #
+      # MD: define macro
+      # RW: read word from memory 538, where position error is stored
+      # IG: if greater
+      # MG: print message
+      # MJ: jump to macro
+      # RL: read long from memory, which is the position in encoder counts. We use this
+      # RP: repeat
+      max_travel = int(limit * enc_counts_per_mm)
+      print('max_travel:', max_travel)
+      self.sendcmds(f'MD501,RW538,IG20,MG"FOUND",MJ505,RL494,IG{max_travel},MG"TOO FAR",MJ505,RP')
 
-    self.sendcmds('MS100')
+      # Stop motion
+      #
+      # MD: define macro
+      # ST: stop
+      self.sendcmds('MD505,ST')
+
+    if(execute):
+      print('Executing softland macro')
+      msg = self.sendcmds('MS500')
+      print(msg)
 
   def close(self):
     if self._port:
